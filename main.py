fd92d7f11c5af3a3fd3f8c3737b4ee7c3d8c3a3e
89645321 <mlove01910@gmail.com>
2024-01-27 16:12:09 +0900

    함수 지우기 전 임시저장

--- main.py
+++ main.py
@@ -16,14 +16,18 @@ users = []
 connections = [{}, {}, {}, {}, {}, {}]
 
 # input json
-autoconnectInput = open("autoconnectInput.json")
+autoconnectInput = open("autoconnectInputExample.json")
 json_object = json.load(autoconnectInput)
 
 for user in json_object["users"]:
+    student_id = int(user.get("col_no"))
+    # 년도가 바뀌면 업데이트해야 함
+    if student_id <= 60:
+        student_id += 100
     users.append(
         User(
             user.get("user_id"),
-            user.get("col_no"),
+            student_id,
             user.get("schedule").get("enter_at").get("major"),
             user.get("schedule").get("exit_at").get("major")
         )
@@ -56,10 +60,31 @@ for i in range(day, last_day):
                 followee = connections[i*2][user.user_id]
             if user.user_id in connections[i*2+1].keys():
                 follower = connections[i*2+1][user.user_id]
-            connections[i*2].pop(user.user_id)
-            connections[i*2].pop(follower)
-            connections[i*2+1].pop(user.user_id)
-            connections[i*2+1].pop(followee)
+            if user.user_id in connections[i*2].keys():
+                connections[i*2].pop(user.user_id)
+            if user.user_id in connections[i*2+1].keys():
+                connections[i*2+1].pop(user.user_id)
+            if follower in connections[i*2].keys():
+                connections[i*2].pop(follower)
+            if followee in connections[i*2+1].keys():
+                connections[i*2+1].pop(followee)
+
+def random_assign(ob, yb, ob_flag, new_connections, follower, command, last_followee):
+    for i in range(len(ob) + len(yb)):
+        if ob_flag:
+            followee = random.choice(ob)
+            ob.remove(followee)
+        else:
+            followee = random.choice(yb)
+            yb.remove(followee)
+        new_connections[follower.user_id] = followee.user_id
+        follower = followee
+        ob_flag = not ob_flag
+
+    if command == "0":
+        new_connections[follower.user_id] = last_followee.user_id
+
+    return new_connections
 
 # 2. 참가자 분류
 # 3. 배정
@@ -91,66 +116,69 @@ else:
 
     # 3. 배정
     new_connections = {}
-    # 원 배정
-    if command == "0":
-        # 반쪽 미배정인 사람들이 있는 경우
-        if len(followee_not_assigned) > 0:
-            # follower, followee만 없는 사람들 중 한 쌍 골라서 빼기
-            follower_random = random.choice(followee_not_assigned)
-            followee_random = random.choice(follower_not_assigned)
-            followee_not_assigned.remove(follower_random)
-            follower_not_assigned.remove(followee_random)
 
-            # follower, followee만 없는 사람들 짝지어주기(위에서 뺀 한 쌍 빼고)
-            for i in range(len(followee_not_assigned)):
-                follower = random.choice(followee_not_assigned)
-                followee_not_assigned.remove(follower)
-                new_connections[follower.user_id] = follower_not_assigned[i].user_id
+    # 반쪽 미배정인 사람들이 있는 경우
+    if len(followee_not_assigned) > 0:
+        # follower, followee만 없는 사람들 중 한 쌍 골라서 빼기
+        follower_random = random.choice(followee_not_assigned)
+        followee_random = random.choice(follower_not_assigned)
+        followee_not_assigned.remove(follower_random)
+        follower_not_assigned.remove(followee_random)
 
-            # 학번별로 ob, yb 배정
-            temp = all_not_assigned.copy()
-            temp.append(follower_random)
-            temp = sorted(temp, key=lambda user: user.col_no)
+        # follower, followee만 없는 사람들 짝지어주기(위에서 뺀 한 쌍 빼고)
+        for i in range(len(followee_not_assigned)):
+            follower = random.choice(followee_not_assigned)
+            followee_not_assigned.remove(follower)
+            new_connections[follower.user_id] = follower_not_assigned[i].user_id
 
-            middle = len(temp) // 2
-            ob_flag = True
-            if (len(temp) % 2) == 1 and temp.index(follower_random) > middle:
-                middle += 1
-            if temp.index(follower_random) < middle:
-                ob_flag = False
-            ob = temp[:middle]
-            yb = temp[middle:]
+        # 학번별로 ob, yb 배정
+        temp = all_not_assigned.copy()
+        temp.append(follower_random)
+        temp = sorted(temp, key=lambda user: user.col_no)
 
-            # cycle 만들기
+        middle = len(temp) // 2
+        ob_flag = True
+        if (len(temp) % 2) == 1 and temp.index(follower_random) <= middle:
+            middle += 1
+        if temp.index(follower_random) < middle:
+            ob_flag = False
+        ob = temp[:middle]
+        yb = temp[middle:]
+
+        # line 만들기
+        if ob_flag:
+            yb.remove(follower_random)
+        else:
+            ob.remove(follower_random)
+        follower = follower_random
+
+        new_connections = random_assign(ob, yb, ob_flag, new_connections, follower, command, followee_random)
+
+        for i in range(len(ob) + len(yb)):
             if ob_flag:
-                yb.remove(follower_random)
+                followee = random.choice(ob)
+                ob.remove(followee)
             else:
-                ob.remove(follower_random)
-            follower = follower_random
+                followee = random.choice(yb)
+                yb.remove(followee)
+            new_connections[follower.user_id] = followee.user_id
+            follower = followee
+            ob_flag = not ob_flag
 
-            for i in range(len(ob) + len(yb)):
-                if ob_flag:
-                    followee = random.choice(ob)
-                    ob.remove(followee)
-                else:
-                    followee = random.choice(yb)
-                    yb.remove(followee)
-                new_connections[follower.user_id] = followee.user_id
-                follower = followee
-                ob_flag = not ob_flag
-
+        # 원배정인 경우에 cycle 만들기
+        if command == "0":
             new_connections[follower.user_id] = followee_random.user_id
 
-        # 반쪽 미배정인 사람들이 없는 경우
-        else:
-            # 배정이 필요하지 않은 경우
-            if len(all_not_assigned) == 0:
-                pass
-            # 한 명 미배정인데 배정해줄 사람이 아무도 없는 경우
-            elif len(all_not_assigned) == 1 and users[1].exit_at < 3:
-                pass
-            # 한 명만 미배정인 경우
-            elif len(all_not_assigned) == 1:
+    # 반쪽 미배정인 사람들이 없는 경우
+    else:
+        # 배정이 필요하지 않은 경우
+        if len(all_not_assigned) == 0:
+            pass
+        # 한 명만 미배정인 경우
+        elif len(all_not_assigned) == 1:
+            # 선 배정이면 아무것도 안 함
+            # 원 배정 중 기존 사이클을 끊고 배정해줄 수 있는 사람이 있는 경우
+            if command == "0" and not (len(users) == 1 or users[1].exit_at < 3):
                 for user in users:
                     if user.user_id != all_not_assigned[0].user_id:
                         follower = user.user_id
@@ -158,70 +186,24 @@ else:
                         new_connections[follower] = all_not_assigned[0].user_id
                         new_connections[all_not_assigned[0].user_id] = followee
                         break
-            # 그 외 일반적인 경우
-            else:
-                # 학번별로 ob, yb 배정
-                all_not_assigned = sorted(all_not_assigned, key=lambda user: user.col_no)
-
-                middle = len(all_not_assigned) // 2
-                ob_flag = True
-                ob = all_not_assigned[:middle]
-                yb = all_not_assigned[middle:]
-
-                # cycle 만들기
-                follower = random.choice(yb)
-                yb.remove(follower)
-                first_follower = follower
-
-                for i in range(len(ob) + len(yb)):
-                    if ob_flag:
-                        followee = random.choice(ob)
-                        ob.remove(followee)
-                    else:
-                        followee = random.choice(yb)
-                        yb.remove(followee)
-                    new_connections[follower.user_id] = followee.user_id
-                    follower = followee
-                    ob_flag = not ob_flag
-
-                new_connections[follower.user_id] = first_follower.user_id
-
-    # 선 배정
-    else:
-        # 반쪽 미배정인 사람들이 있는 경우
-        if len(followee_not_assigned) > 0:
-            # follower, followee만 없는 사람들 중 한 쌍 골라서 빼기
-            follower_random = random.choice(followee_not_assigned)
-            followee_random = random.choice(follower_not_assigned)
-            followee_not_assigned.remove(follower_random)
-            follower_not_assigned.remove(followee_random)
-
-            # follower, followee만 없는 사람들 짝지어주기(위에서 뺀 한 쌍 빼고)
-            for i in range(len(followee_not_assigned)):
-                follower = random.choice(followee_not_assigned)
-                followee_not_assigned.remove(follower)
-                new_connections[follower.user_id] = follower_not_assigned[i].user_id
-
+        # 그 외 일반적인 경우
+        else:
             # 학번별로 ob, yb 배정
-            temp = all_not_assigned.copy()
-            temp.append(follower_random)
-            temp = sorted(temp, key=lambda user: user.col_no)
+            all_not_assigned = sorted(all_not_assigned, key=lambda user: user.col_no)
 
-            middle = len(temp) // 2
+            middle = len(all_not_assigned) // 2
             ob_flag = True
-            if (len(temp) % 2) == 1 and temp.index(follower_random) > middle:
-                middle += 1
-            if temp.index(follower_random) < middle:
-                ob_flag = False
-            ob = temp[:middle]
-            yb = temp[middle:]
+            ob = all_not_assigned[:middle]
+            yb = all_not_assigned[middle:]
 
             # cycle 만들기
-            if ob_flag:
-                yb.remove(follower_random)
-            else:
-                ob.remove(follower_random)
-            follower = follower_random
+            follower = random.choice(yb)
+            yb.remove(follower)
+
+            new_connections = random_assign(ob, yb, ob_flag, new_connections, follower, command, follower)
+
+            if command == "0":
+                first_follower = follower
 
             for i in range(len(ob) + len(yb)):
                 if ob_flag:
@@ -234,35 +216,8 @@ else:
                 follower = followee
                 ob_flag = not ob_flag
 
-        # 반쪽 미배정인 사람들이 없는 경우
-        else:
-            # 배정이 필요하지 않은 경우
-            if len(all_not_assigned) == 0:
-                pass
-            # 한 명만 미배정인 경우
-            elif len(all_not_assigned) == 1:
-                pass
-            # 그 외 일반적인 경우
-            else:
-                # 학번별로 ob, yb 배정
-                all_not_assigned = sorted(all_not_assigned, key=lambda user: user.col_no)
+            if command == "0":
+                new_connections[follower.user_id] = first_follower.user_id
 
-                middle = len(all_not_assigned) // 2
-                ob_flag = True
-                ob = all_not_assigned[:middle]
-                yb = all_not_assigned[middle:]
-
-                # cycle 만들기
-                follower = random.choice(yb)
-                yb.remove(follower)
-
-                for i in range(len(ob) + len(yb)):
-                    if ob_flag:
-                        followee = random.choice(ob)
-                        ob.remove(followee)
-                    else:
-                        followee = random.choice(yb)
-                        yb.remove(followee)
-                    new_connections[follower.user_id] = followee.user_id
-                    follower = followee
-                    ob_flag = not ob_flag
+for connection in new_connections:
+    print(connection, new_connections[connection])
